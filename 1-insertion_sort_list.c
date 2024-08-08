4ac638eff2c8b3e361c90c22c664e32aa8a9e360
Mauriciourrozz <9366@holberonstudents.com>
2024-08-08 18:48:02 +0000

    2

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,25 @@
 #include "sort.h"
+#include <stdio.h>
 /**
  *
  */
 void insertion_sort_list(listint_t **list)
-{}
+{
+	listint_t *current = *list
+
+	current = current->next;
+
+	while (current != NULL)
+	{
+		if (current->next != NULL && current->n > current->prev->n)/*pregunto si el siguiente no es null para evitar segmentation fault*/
+		{
+			(*list)->next = current->next->prev;
+			(*list)->prev = current;
+			current->prev = (*list);
+			(*list)->next->prev = (*list);
+			current->prev = NULL;
+			print_list(*list);
+		}
+		current = current->next;/*para avanzar en el while*/
+	}
+}
