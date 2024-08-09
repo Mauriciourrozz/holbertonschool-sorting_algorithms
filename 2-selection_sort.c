3f46da60830e8a5cf24650572e8a11a9b21e2ca4
Mauriciourrozz <9366@holbertonstudents.com>
2024-08-09 14:35:51 +0000

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,41 @@
 #include "sort.h"
 /**
- *
+ * swap - Intercambia los numeros
+ * @a: numero
+ * @b: numero
+ * Return: Void
  */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
+ * selection_sort - Ordena una lista usando la selection sort
+ * @array: Lista de numeros
+ * @size: tamaño de la lista
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t pass;
+	size_t minor;
+	size_t i;
 
+	for (pass = 0; pass < size - 1; pass++)
+	{
+		minor = pass;
+		for (i = pass + 1; i < size; i++)
+		{
+			if (array[i] < array[minor])
+			{
+				minor = i;
+			}
+		}
+		if (minor != pass)
+		{
+			swap(&array[pass], &array[minor]);
+			print_array(array, size);
+		}
+	}
+}
