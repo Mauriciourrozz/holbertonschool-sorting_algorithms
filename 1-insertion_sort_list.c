bf6789908b7be2573a3db547912a4d691eda9d4c
kimikoultramega <9379@holbertonstudents.com>
2024-08-12 00:42:49 +0000

    Mauri fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,25 +1,53 @@
 #include "sort.h"
 #include <stdio.h>
 /**
- *
+ * insertion_sort_list - list
+ * @list: list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = *list
-
-	current = current->next;
+	if (list == NULL || *list == NULL)
+	{
+		return;
+	}
+	listint_t *current = (*list)->next;
+	listint_t *siguiente_a_current = current->next;
+	listint_t *anterior_a_current = current->prev;
 
 	while (current != NULL)
 	{
-		if (current->next != NULL && current->n > current->prev->n)/*pregunto si el siguiente no es null para evitar segmentation fault*/
+		while (anterior_a_current != NULL && anterior_a_current->n > current->n)
 		{
-			(*list)->next = current->next->prev;
-			(*list)->prev = current;
-			current->prev = (*list);
-			(*list)->next->prev = (*list);
-			current->prev = NULL;
-			print_list(*list);
+			anterior_a_current = anterior_a_current->prev;
 		}
-		current = current->next;/*para avanzar en el while*/
-	}
+		if (anterior_a_current != current->prev)
+		{
+			if (current->prev != NULL)
+			{
+				current->prev->next = current->next;
+			}
+			if (current->next != NULL)
+			{
+				current->next->prev = current->prev;
+			}
+			if (anterior_a_current == NULL)
+			{
+				current->prev = NULL;
+				current->next = *list;
+				(*list)->prev = current;
+				*list = current;
+			}
+			else
+			{
+				current->prev = anterior_a_current;
+				current->next = anterior_a_current->next;
+				if (anterior_a_current->next != NULL)
+				{
+					anterior_a_current->next->prev = current;
+					anterior_a_current->next = current;
+				}
+				print_list(*list);
+			}
+			current = siguiente_a_current;
+		}
 }
